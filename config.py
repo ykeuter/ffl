f0f700262e0d25472fb873b8686a1fceaf113f72
Yvo Keuter <ykeuter@me.com>
2017-07-31 16:50:04 +0200

    changed installed packages for heroku

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
+import os
+
 DEBUG = True
 SECRET_KEY = 'ro6W6BTzPw29'
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:7tiR9H0NR7Bq@localhost/ffl"
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:7tiR9H0NR7Bq@localhost/ffl"
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 POSITIONS_FILE = "data/positions.csv"
 TEAMS_FILE = "data/teams.csv"
