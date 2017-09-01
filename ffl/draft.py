1e692a55ca08c97219a68d532c344d16fdf411a7
Yvo Keuter <ykeuter@me.com>
2017-09-01 18:15:00 +0500

    draft logic working

--- ffl/draft.py
+++ ffl/draft.py
@@ -1,12 +1,92 @@
-from ffl import models
+class FreeAgent:
+    def __init__(self, id, name, team, positions, points):
+        self.id = id
+        self.name = name
+        self.team = team
+        self.positions = positions
+        self.points = points
 
-STARTERS = {"QB": 1, "WR": 2, "RB": 2, "FLEX": 1, "TE": 1, "EDR": 1, "D": 1,
-        "K": 1}
-
-class state():
-    def __init__(self, rosters, turns, freeagents):
+class GameState:
+    def __init__(self, rosters, turns, freeagents, playerjm=None):
         self.rosters = rosters
         self.turns = turns
-        self.freeagents = freeagents
+        self.freeagents = sorted(freeagents, key=lambda p: -p.points)
+        self.playerJustMoved = playerjm
 
+    def Clone(self):
+        """ Create a deep clone of this game state.
+        """
+        rosters = map(lambda r: r[:], self.rosters)
+        st = GameState(rosters, self.turns[:], self.freeagents[:],
+                self.playerJustMoved)
+        return st
 
+    def DoMove(self, move):
+        """ Update a state by carrying out the given move.
+            Must update playerJustMoved.
+        """
+        pick = next(p for p in self.freeagents if move in p.positions)
+        player = self.turns.pop(0)
+        self.rosters[player].append(pick)
+        self.freeagents.remove(pick)
+        self.playerJustMoved = player
+
+    def GetMoves(self):
+        """ Get all possible moves from this state.
+        """
+        MAX_POS = {"QB": 3, "WR": 8, "RB": 8, "TE": 2, "EDR": 2, "D": 2, "K": 2}
+
+        if len(self.turns) == 0: return []
+
+        roster = self.rosters[self.turns[0]]
+        moves = [k for (k, v) in MAX_POS.iteritems() if len([p for p in roster if (k in
+            p.positions)]) < v]
+        # moves2 = reduce(set.union, [p.positions for p in self.freeagents], set())
+        # moves = set(moves).intersection(moves2)
+        return list(moves)
+
+    def GetResult(self, playerjm):
+        """ Get the game result from the viewpoint of playerjm.
+        """
+        WEIGHTS_POS = [(["QB"], .7),
+                       (["WR"], .7),
+                       (["WR"], .7),
+                       (["RB"], .7),
+                       (["RB"], .7),
+                       (["TE"], .6),
+                       (["RB", "WR", "TE"], .6),
+                       (["EDR"], .6),
+                       (["D"], .7),
+                       (["K"], .6),
+                       (["QB"], .3),
+                       (["WR"], .4),
+                       (["RB"], .4),
+                       (["TE"], .4),
+                       (["RB", "WR", "TE"], .4),
+                       (["EDR"], .4),
+                       (["D"], .3),
+                       (["K"], .4),
+                       (["WR"], .2),
+                       (["RB"], .2)]
+
+        if playerjm is None: return 0
+
+        roster = sorted(self.rosters[playerjm], key=lambda p: -p.points)
+        res = 0
+        for (pos, w) in WEIGHTS_POS:
+            p = next((p for p in roster if set(p.positions).intersection(pos)), None)
+            if p:
+                points = p.points
+                roster.remove(p)
+            else:
+                ps = [p.points for p in self.freeagents if
+                        set(p.positions).intersection(pos)]
+                if len(ps) > 3: ps = ps[:3]
+                points = float(sum(ps)) / max(len(ps), 1)
+            res += points * w
+        return res
+
+    def __repr__(self):
+        """ Don't need this - but good style.
+        """
+        pass
---
+++ ffl/mcts.py
@@ -0,0 +1,144 @@
+import random, math
+
+class Node:
+    """ A node in the game tree. Note wins is always from the viewpoint of playerJustMoved.
+        Crashes if state not specified.
+    """
+    def __init__(self, move = None, parent = None, state = None):
+        self.move = move # the move that got us to this node - "None" for the root node
+        self.parentNode = parent # "None" for the root node
+        self.childNodes = []
+        self.score = 0
+        self.visits = 0
+        self.untriedMoves = state.GetMoves() # future child nodes
+        self.playerJustMoved = state.playerJustMoved # the only part of the state that the Node needs later
+
+    def UCTSelectChild(self):
+        """ Use the UCB1 formula to select a child node. Often a constant UCTK is applied so we have
+            lambda c: c.wins/c.visits + UCTK * sqrt(2*log(self.visits)/c.visits to vary the amount of
+            exploration versus exploitation.
+        """
+        UCTK = 10
+        s = sorted(self.childNodes, key = lambda c: c.score/c.visits + UCTK *
+                math.sqrt(2*math.log(self.visits)/c.visits))[-1]
+        return s
+
+    def AddChild(self, m, s):
+        """ Remove m from untriedMoves and add a new child node for this move.
+            Return the added child node
+        """
+        n = Node(move = m, parent = self, state = s)
+        self.untriedMoves.remove(m)
+        self.childNodes.append(n)
+        return n
+
+    def Update(self, result):
+        """ Update this node - one additional visit and result additional wins.
+            result must be from the viewpoint of playerJustmoved.
+        """
+        self.visits += 1
+        self.score += result
+
+    def __repr__(self):
+        return "[M:" + str(self.move) + " S:" + str(self.score/self.visits) + " U:" + str(self.untriedMoves) + "]"
+
+    def TreeToString(self, indent):
+        s = self.IndentString(indent) + str(self)
+        for c in self.childNodes:
+             s += c.TreeToString(indent+1)
+        return s
+
+    def IndentString(self,indent):
+        s = "\n"
+        for i in range (1,indent+1):
+            s += "| "
+        return s
+
+    def ChildrenToString(self):
+        s = ""
+        for c in self.childNodes:
+             s += str(c) + "\n"
+        return s
+
+
+def UCT(rootstate, itermax, verbose = False):
+    """ Conduct a UCT search for itermax iterations starting from rootstate.
+        Return the best move from the rootstate.
+    """
+    rootnode = Node(state = rootstate)
+
+    for i in range(itermax):
+        node = rootnode
+        state = rootstate.Clone()
+
+        # Select
+        while node.untriedMoves == [] and node.childNodes != []: # node is fully expanded and non-terminal
+            node = node.UCTSelectChild()
+            state.DoMove(node.move)
+
+        # Expand
+        if node.untriedMoves != []: # if we can expand (i.e. state/node is non-terminal)
+            m = random.choice(node.untriedMoves)
+            state.DoMove(m)
+            node = node.AddChild(m,state) # add child and descend tree
+
+        # Rollout - this can often be made orders of magnitude quicker using a state.GetRandomMove() function
+        while state.GetMoves() != []: # while state is non-terminal
+            state.DoMove(random.choice(state.GetMoves()))
+
+        # Backpropagate
+        while node != None: # backpropagate from the expanded node and work back to the root node
+            node.Update(state.GetResult(node.playerJustMoved))
+                # state is terminal. Update node with result from POV of node.playerJustMoved
+            node = node.parentNode
+
+    # Output some information about the tree - can be omitted
+    if (verbose): print rootnode.TreeToString(0)
+    # else: print rootnode.ChildrenToString()
+
+    return sorted(rootnode.childNodes, key = lambda c: c.score / c.visits)[-1].move
+
+def UCTPlayGame(state):
+    """ Play a sample game between two UCT players where each player gets a different number
+        of UCT iterations (= simulations = tree nodes).
+    """
+    # state = OthelloState(4) # uncomment to play Othello on a square board of the given size
+    # state = OXOState() # uncomment to play OXO
+    # state = NimState(15) # uncomment to play Nim with the given number of starting chips
+    while (state.GetMoves() != []):
+        # print str(state)
+        # if state.playerJustMoved == 1:
+        m = UCT(rootstate = state, itermax = 100, verbose = False) # play with values for itermax and verbose = True
+        # else:
+        #     m = UCT(rootstate = state, itermax = 100, verbose = False)
+        print "Best Move: " + str(m) + "\n"
+        state.DoMove(m)
+    # if state.GetResult(state.playerJustMoved) == 1.0:
+    #     print "Player " + str(state.playerJustMoved) + " wins!"
+    # elif state.GetResult(state.playerJustMoved) == 0.0:
+    #     print "Player " + str(3 - state.playerJustMoved) + " wins!"
+    # else: print "Nobody wins!"
+    return state
+
+if __name__ == "__main__":
+    """ Play a single game to the end using UCT for both players. 
+    """
+    from ffl import models, draft
+
+    NUM = 8
+    SIZE = 10
+    FA_STRING = "FA"
+    DEF_STRING = "D"
+    fa = [draft.FreeAgent(p.espn_id, p.name,
+        p.team.espn_code if p.team else FA_STRING,
+        [pos.espn_code for pos in p.positions],
+        p.projected_points) for p in models.NflPlayer.query.all()]
+    fa += [draft.FreeAgent(t.espn_id, t.name, t.espn_code, [DEF_STRING],
+        t.projected_defense_points) for t in
+        models.NflTeam.query.all()]
+    rosters = [[] for _ in range(NUM)]
+    turns = []
+    for i in range(SIZE):
+        turns += range(NUM) if i % 2 else reversed(range(NUM))
+    state = draft.GameState(rosters, turns, fa)
+    UCTPlayGame(state)
